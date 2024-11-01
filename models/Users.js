44371f00b6ef988529b340bb5ed3c9248de56693
Xavier-Msrt <xaviermassart@outlook.com>
2024-11-01 11:42:07 +0100

    feat: add get one user

--- models/Users.js
+++ models/Users.js
@@ -14,6 +14,7 @@ userSchema.set('toJSON', {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
+        delete returnedObject.password
     }
 })
 
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/Users');
 
-
+// GET All /users
 router.get('/', (req, res) => {
     User.find().then((users) => {
         res.json(users);
@@ -11,7 +11,14 @@ router.get('/', (req, res) => {
 });
 
 
-
+// GET One /users/:id
+router.get('/:id', (req, res) => {
+    User.findById(req.params.id).then((user) => {
+        res.json(user);
+    }).catch(() => {
+        res.sendStatus(404);
+    });
+});
 
 
 module.exports = router;
