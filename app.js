dd8207ed20eab617d9b7c451780373ef200b234d
xavier-massart-vinci <xavier.massart@student.vinci.be>
2024-11-04 23:09:48 +0100

    feat: add cors rules and doc

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auths');
@@ -9,6 +11,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+var corsOptions = {
+  origin: process.env.NODE_ENV === 'production' 
+  ? process.env.PRODUCTION_ORIGIN : '*'
+}
+
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
