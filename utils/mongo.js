2eb6c82482c5e11ee80477331dd6a739970e5f93
Alexis <alexis.cattaruzza@student.vinci.be>
2024-11-06 14:52:30 +0100

    add space mongo.js

--- utils/mongo.js
+++ utils/mongo.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
+
 mongoose.set('strictQuery', false)
 const url = process.env.MONGODB
+
+
 
 const connect = () => {
     mongoose.connect(url)
