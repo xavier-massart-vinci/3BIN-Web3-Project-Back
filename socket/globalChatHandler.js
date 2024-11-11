100d602fe76042a7c1141c9660002195fe3a663b
xassart-massart-vinci <xavier.massart@student.vinci.be>
2024-11-11 17:57:07 +0100

    fix: space

--- socket/globalChatHandler.js
+++ socket/globalChatHandler.js
@@ -1,10 +1,7 @@
 module.exports = (io) =>{
-
     const globalChat = function(msg) {
         const socket = this;
-
         io.emit('globalChatMessage', {content: msg.content, user: socket.user.__id, type: "text"});
-        console.log('message: ' + msg + ' from ' + socket.user.username);
     };
 
     return globalChat;
--- socket/privateChatHandler.js
+++ socket/privateChatHandler.js
@@ -1,9 +1,8 @@
 module.exports = (io) =>{
-
     const privateChat = function(msg) {
         const socket = this;
         socket.to(msg.for).emit("privateChatMessage", msg);
     };
-
+    
     return privateChat;
 };
