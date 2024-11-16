27be3f07bade88f09902d231ed1bdbd8b08340d0
xavier-massart-vinci <xavier.massart@student.vinci.be>
2024-11-16 14:15:33 +0100

    chore: rename env uri

--- services/mongo.js
+++ services/mongo.js
@@ -1,10 +1,16 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
-const url = process.env.MONGODB
+
+const mongoURI = process.env.MONGO_URI;
+
+if (!mongoURI) {
+    console.error("MONGO_URI is not defined in the environment");
+    process.exit(1);
+}
 
 const connect = () => {
-    mongoose.connect(url)
+    mongoose.connect(mongoURI)
     .catch(error => {
         console.log('error connecting to MongoDB:', error.message)
         process.exit(1);
