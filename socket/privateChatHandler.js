5917b36326bfec84d49bdf8c8e2179df832219e2
Alexis <alexis.cattaruzza@student.vinci.be>
2024-11-18 10:36:47 +0100

    add: check if friend when send message

--- socket/privateChatHandler.js
+++ socket/privateChatHandler.js
@@ -5,13 +5,27 @@ module.exports = (io) =>{
     const privateChat = async function(msg) {
         const socket = this;
         let toSocket = users.getUser(msg.to);
+        console.log("toSocket", toSocket);
 
         // TODO check if receiver is a friend of the sender
-        
+        // Get the sender and receiver id
+        const sender = socket.user;
+        const receiverId = msg.to;
+        console.log("sender", sender);
+        console.log("receiverId", receiverId);
 
+ 
+        // Check if the sender and receiver are friends
+        const isFriend = sender.friends.includes(receiverId);
+        console.log("isFriend", isFriend);
+        if (!isFriend) {
+            return; 
+        } 
 
         // socket send the message to the receiver
-        socket.to(toSocket).emit("privateChatMessage", msg); // Send the message to the receiver
+        if(toSocket){
+            socket.to(toSocket).emit("privateChatMessage", msg); // Send the message to the receiver
+        }
         socket.emit("privateChatMessage", msg); // Send the message to the sender
         
         // save message in the database
